a3b8e090a0c11e9552c41b5c6642a9d54f9fce4a
Jacob Powers <jpowers@23andme.com>
2015-10-24 15:03:56 -0700

    Update setup.sh

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,11 @@
 #!bin/bash
+# run this script in ~/dotfiles/
 
 sh .gitaliases.sh
 
-ln -sf ./.tmux.conf ~/.tmux.conf
-ln -sf ./.vimrc ~/.vimrc
+SOURCEDIR=$PWD
+TARGETDIR=$(dirname $PWD)
+
+ln -sf $SOURCEDIR/.tmux.conf $TARGETDIR/.tmux.conf
+ln -sf $SOURCEDIR/.vimrc $TARGETDIR/.vimrc
+

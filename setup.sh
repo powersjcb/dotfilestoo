c82c1c2a3c5e4d735a30f49b7c369f1fa9ded522
Jacob Powers <jpowers@23andme.com>
2015-10-24 18:29:40 -0700

    Add zsh install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,34 @@
 sh symlink.sh
 
-zsh
+if [[ -x "/usr/bin/apt-get" ]]; then
+    echo "apt-get detected"
+    INSTALL_METHOD="apt-get"
+    sudo apt-get update
+elif [[ -x "/usr/bin/yum" ]]; then
+    echo "yum detected"
+    INSTALL_METHOD="yum"
+    yum check-update
+elif which programname >/dev/null; then
+    echo "Homebrew detected"
+    INSTALL_METHOD="brew"
+    brew update
+else
+    echo "could not find installer"
+    exit 1 
+fi
+
+try_install () {
+    {
+        echo "Tring to install $1"
+        $INSTALL_METHOD install $1
+    } || {
+        echo "Failed to install $1"
+    }
+}
+
+try_install "zsh"
+
+
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB

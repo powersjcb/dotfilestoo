c09d1830a0ebd6594dde1a65989d84c8d521da89
Jacob Powers <jpowers@23andme.com>
2015-12-22 14:17:01 -0800

    Add start working on adding vim compilation

--- dependencies.sh
+++ dependencies.sh
@@ -10,7 +10,8 @@ trap "echo 'Removing temp directory'; rm -rf $TEMP_DIR;" EXIT
 wget -O $TEMP_DIR/zsh-5.2.tar.gz http://sourceforge.net/projects/zsh/files/zsh/5.2/zsh-5.2.tar.gz/download
 tar -zxvf $TEMP_DIR/zsh-5.2.tar.gz
 
-LOCAL_DIR=$HOME/local/
+# put everything inside of temp dir to be symlinked when done
+LOCAL_DIR=$TEMP_DIR/local/
 mkdir $LOCAL_DIR
 
 cd zsh-5.2
@@ -19,5 +20,24 @@ make && make test && make install
 
 chsh -s $LOCAL_DIR/bin/zsh
 
+# Install rbenv and then ruby 2.2.2
+git clone --depth 1 https://github.com/rbenv/rbenv.git $TEMP_DIR/.rbenv
+
+# Download and install newest vim
+
+git clone --depth 1 https://github.com/vim/vim.git 
+cd vim
+./configure --with-features=huge \
+    --enable-multibyte \
+    --enable-rubyinterp \
+    --enable-pythoninterp \
+    --enable-perlinterp \
+    --enable-rubyinterp \
+    --enable-luainterp \
+    --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim74
+sudo make install
 
 
+# Last step should be to add all paths
+#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.bash_profile
